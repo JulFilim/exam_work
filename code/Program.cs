de49d0989c7381ea27418092ad1f1c92532f3497
JulFilim <julfilim@mail.ru>
2023-06-24 17:18:47 +0300

    added metods Number,InputMass,CreateArray

---
+++ code/Program.cs
@@ -0,0 +1,21 @@
+﻿int Number()
+{
+    int num = int.Parse(Console.ReadLine());
+    return num;
+    Console.WriteLine();
+}
+
+string InputMass()
+{
+    string mass = Console.ReadLine();
+    return mass;
+    Console.WriteLine();
+}
+void CreateArray(string[] mass, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        mass[i] = InputMass();
+    }
+    Console.WriteLine();
+}
